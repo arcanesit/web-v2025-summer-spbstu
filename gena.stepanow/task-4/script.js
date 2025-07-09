682271246cc688964ebff7a4119d08fee07d5299
arcanesit <stepanow.gena.r@gmail.com>
2025-07-09 19:09:22 +0300

    [TASK-4] Class User layout and task decription

---
+++ gena.stepanow/task-4/script.js
@@ -0,0 +1,13 @@
+class User{
+    constructor(id, name, friends)
+    {
+        this.id = id;
+        this.name = name;
+        this.friends = friends;
+    }
+    
+    addFriend(friendId){}
+    removeFriend(friendId){}
+    get friendCount(){}
+
+}
